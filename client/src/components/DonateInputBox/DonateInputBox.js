13dfa31623bab37317c6e794ec9d1f8e8c81cc9b
Nathan Jahnke <naterased@gmail.com>
2018-04-27 23:16:57 -0500

    im so confused

--- client/src/components/DonateInputBox/DonateInputBox.js
+++ client/src/components/DonateInputBox/DonateInputBox.js
@@ -4,45 +4,60 @@ import "./DonateInputBox.css";
 import { Col, Row, Container } from "../../components/Grid";
 import Barcode from "react-barcode";
 import axios from "axios";
-var createClass = require('create-react-class');
-
+var createClass = require("create-react-class");
 
 class DonateInputBox extends Component {
-    // Setting the component's initial state
-    state = {
-        value: "",
-    };
+  // Setting the component's initial state
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
 
-    handleInputChange = event => {
-        this.setState({ value: event.target.value });
-    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
 
+  handleInputChange = event => {
+    this.setState({ value: event.target.value });
+  };
 
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <div className="input-group mb-3 mx-auto">
-                        <input
-                            type="text"
-                            value={this.state.value}
-                            className="form-control"
-                            placeholder="Enter ISBN#"
-                            aria-label="Enter ISBN#"
-                            aria-describedby="basic-addon2"
-                            onChange={this.handleInputChange}
-                        />
-                        <div className="input-group-append">
-                            <button className="btn btn-outline-secondary" type="button">Search</button>
-                        </div>
-                    </div>
-                </Row>
-                <Row>
-                    <Barcode value={this.state.value} />
-                </Row>
-            </Container>
-        );
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.props.onSubmit && typeof this.props.onSubmit === "function") {
+      this.props.onSubmit(this.state.value);
     }
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <div className="input-group mb-3 mx-auto">
+            <input
+              type="text"
+              value={this.state.value}
+              className="form-control"
+              placeholder="Enter ISBN#"
+              aria-label="Enter ISBN#"
+              aria-describedby="basic-addon2"
+              onChange={this.handleInputChange}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={props.handleFormSubmit}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </Row>
+        <Row>
+          <Barcode value={this.state.value} />
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default DonateInputBox;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 export default {
   // Sample API1
-  api1: function() {
-    return axios.get();
+  getBook: function(isbn) {
+    return axios.get(
+      "https://www.googleapis.com/books/v1/volumes?q=isbn:" +
+        isbn +
+        "&key=AIzaSyBr1902s5dmz5vSbYgt67qn3x0HO7rMmzg"
+    );
   }
-
 };
