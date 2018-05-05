6dfe583c5bae35755769542e914b50811230ab5e
idaselma3025 <idaselma3025@gmail.com>
2018-05-05 12:56:33 -0500

    manage

--- client/src/pages/Manage/Manage.js
+++ client/src/pages/Manage/Manage.js
@@ -4,6 +4,7 @@ import { Col, Row, Container } from "../../components/Grid";
 import API from "../../utils/API";
 import UserIcon from "../../components/UserIcon";
 import BookCard from "../../components/BookCard";
+import DeleteBtn from "../../components/DeleteBtn";
 
 
 class Manage extends Component {
@@ -12,15 +13,22 @@ class Manage extends Component {
   };
 
 componentDidMount(){
-  this.loadBooks()
+  this.loadBooks();
 };
 
+
 loadBooks = userId => {
-  API.getUserBooks("5ae7c15591c8bf04aefc9be8")
+  API.getUserBooks("5aedca116521850c59d4a680")
   .then(res => {
     this.setState({books:res.data})
   })
   .catch(err => console.log(err));
+};
+
+deleteBook = id => {
+  API.deleteBook(id)
+    .then(res => this.loadBooks())
+    .catch(err => console.log(err));
 };
 
 // handleInputChange = event => {
@@ -43,7 +51,11 @@ render() {
         {this.state.books.length ? (
           <Col size="md-12 lg-12">
             {this.state.books.map(book => (
-              <BookCard book={book} key={book._id}></BookCard>
+              <div>
+              <BookCard book={book} key={book._id} source={"manage"}>
+    
+              </BookCard>
+              </div>
             ))}
           </Col>
         ) : (
