7e5456af27e52d36e9c450f0f849b0243eeb77e9
Nathan Jahnke <naterased@gmail.com>
2018-05-06 11:59:01 -0500

    still working

--- client/src/components/BookDonateModal/BookDonateModal.js
+++ client/src/components/BookDonateModal/BookDonateModal.js
@@ -5,29 +5,53 @@ import { Col, Row, Container } from "../../components/Grid";
 //we are using bootstrap 4, the modal does not work. Hence using library react-bootstrap4-modal
 // import Modal from 'react-bootstrap/lib/Modal';
 //Libraries from react-bootstrap (npm install --save react-bootstrap4-modal )
-import Modal from 'react-bootstrap4-modal';
-import Button from 'react-bootstrap/lib/Button';
+import Modal from "react-bootstrap4-modal";
+import Button from "react-bootstrap/lib/Button";
+import DeleteBtn from "../../components/DeleteBtn";
+import Manage from "../../pages/Manage";
+import API from "../../utils/API";
+import DonateBookCard from "../../components/DonateBookCard";
 
 class BookDonateModal extends Component {
-    render() {
-        return (
-            //This modal needs to be enhanced to show more book and donor details
-            <Modal {...this.props}>
-                <div className="modal-header">
-                    <h5 className="modal-title">{this.props.title}</h5>
-                </div>
-                <div className="modal-body">
-                    <p>{this.props.author}</p>
-                    <p>{this.props.desc}</p>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" onClick={this.props.onHide}>
-                        Close
-                    </button>
-                </div>
-            </Modal>
-        );
-    }
+  deleteBook = id => {
+    API.deleteBook(id).catch(err => console.log(err));
+  };
+  render() {
+    return; //This modal needs to be enhanced to show more book and donor details
+    <Modal>
+      <div className="modal-header">
+        <h5 className="modal-title">{this.props.title}</h5>
+      </div>
+      <div className="modal-body">
+        {/* <DonateBookCard
+          bookImage={this.state.bookImage}
+          author={this.state.author}
+          title={this.state.title}
+          buttonTitle={"Donate Book"}
+          buttonClick={this.postBookDB}
+        /> */}
+        <p>{this.props.author}</p>
+        <p>{this.props.desc}</p>
+      </div>
+      <div className="modal-footer">
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={this.props.onClickBackdrop}
+        >
+          Cancel
+        </button>
+        <DeleteBtn onClick={this.deleteBook(this.props._id)} />
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={this.props.onHide}
+        >
+          Edit
+        </button>
+      </div>
+    </Modal>;
+  }
 }
 
 export default BookDonateModal;
--- client/src/pages/Manage/Manage.js
+++ client/src/pages/Manage/Manage.js
@@ -6,66 +6,61 @@ import UserIcon from "../../components/UserIcon";
 import BookCard from "../../components/BookCard";
 import DeleteBtn from "../../components/DeleteBtn";
 
-
 class Manage extends Component {
   state = {
-    books : []
+    books: []
   };
 
-componentDidMount(){
-  this.loadBooks();
-};
+  componentDidMount() {
+    this.loadBooks();
+  }
 
+  loadBooks = userId => {
+    API.getUserBooks("5aee43b64e2c233d9caabe5e")
+      .then(res => {
+        this.setState({ books: res.data });
+      })
+      .catch(err => console.log(err));
+  };
 
-loadBooks = userId => {
-  API.getUserBooks("5aedca116521850c59d4a680")
-  .then(res => {
-    this.setState({books:res.data})
-  })
-  .catch(err => console.log(err));
-};
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
 
-deleteBook = id => {
-  API.deleteBook(id)
-    .then(res => this.loadBooks())
-    .catch(err => console.log(err));
-};
+  // handleInputChange = event => {
+  //   const { name, value } = event.target;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
 
-// handleInputChange = event => {
-//   const { name, value } = event.target;
-//   this.setState({
-//     [name]: value
-//   });
-// };
-
-
-render() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col size="lg-12 md-12 sm-12">
-          <hr></hr>
-        </Col>
-      </Row>
-      <Row>
-        {this.state.books.length ? (
-          <Col size="md-12 lg-12">
-            {this.state.books.map(book => (
-              <div>
-              <BookCard book={book} key={book._id} source={"manage"}>
-    
-              </BookCard>
-              </div>
-            ))}
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="lg-12 md-12 sm-12">
+            <hr />
           </Col>
-        ) : (
-          <h3>No Results to Display</h3>
-        )}
-      </Row>
-      <div className="push"></div>
-    </Container>
-  );
-}
+        </Row>
+        <Row>
+          {this.state.books.length ? (
+            <Col size="md-12 lg-12">
+              {this.state.books.map(book => (
+                <div>
+                  <BookCard book={book} key={book._id} source={"manage"} />
+                </div>
+              ))}
+            </Col>
+          ) : (
+            <h3>No Results to Display</h3>
+          )}
+        </Row>
+        <div className="push" />
+      </Container>
+    );
+  }
 }
 
 export default Manage;
