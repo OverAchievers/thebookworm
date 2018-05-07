6393631bb78089d7e69ff2cc1238e67244edd2a6
mrderrickguy <zakiahdg@gmail.com>
2018-05-07 17:44:47 -0500

    commit

--- client/src/pages/Manage/Manage.js
+++ client/src/pages/Manage/Manage.js
@@ -8,52 +8,52 @@ import BookCard from "../../components/BookCard";
 
 class Manage extends Component {
   state = {
-    books : []
+    books: []
   };
 
-componentDidMount(){
-  this.loadBooks()
-};
+  componentDidMount() {
+    this.loadBooks()
+  };
 
-loadBooks = userId => {
-  API.getUserBooks("5ae7c15591c8bf04aefc9be8")
-  .then(res => {
-    this.setState({books:res.data})
-  })
-  .catch(err => console.log(err));
-};
+  loadBooks = userId => {
+    API.getUserBooks("5aede203ea3cd12fc0e7c1c2")
+      .then(res => {
+        this.setState({ books: res.data })
+      })
+      .catch(err => console.log(err));
+  };
 
-// handleInputChange = event => {
-//   const { name, value } = event.target;
-//   this.setState({
-//     [name]: value
-//   });
-// };
+  // handleInputChange = event => {
+  //   const { name, value } = event.target;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
 
 
-render() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col size="lg-12 md-12 sm-12">
-          <hr></hr>
-        </Col>
-      </Row>
-      <Row>
-        {this.state.books.length ? (
-          <Col size="md-12 lg-12">
-            {this.state.books.map(book => (
-              <BookCard book={book} key={book._id}></BookCard>
-            ))}
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="lg-12 md-12 sm-12">
+            <hr></hr>
           </Col>
-        ) : (
-          <h3>No Results to Display</h3>
-        )}
-      </Row>
-      <div className="push"></div>
-    </Container>
-  );
-}
+        </Row>
+        <Row>
+          {this.state.books.length ? (
+            <Col size="md-12 lg-12">
+              {this.state.books.map(book => (
+                <BookCard book={book} key={book._id}></BookCard>
+              ))}
+            </Col>
+          ) : (
+              <h3>No Results to Display</h3>
+            )}
+        </Row>
+        <div className="push"></div>
+      </Container>
+    );
+  }
 }
 
 export default Manage;
