a30afe1598fc01a9534460ff622b810db5fbfecf
Derek <d.frand@yahoo.com>
2018-05-06 20:39:56 -0500

    HomeResponsive

--- client/src/components/Browsebtn/Browsebtn.js
+++ client/src/components/Browsebtn/Browsebtn.js
@@ -3,7 +3,7 @@ import "./Browsebtn.css";
 
 const Browsebtn = props => (
 <div className="button-container">
-		 <h4 className="browse-para">    Looking for a book?  </h4>
+		 <h4 className="browse-para">Looking for a book?</h4>
 			<a className="browse-btn" onClick={() => props.whenClicked('/browse')}>
 			<img
 			 src={"https://media.giphy.com/media/SU1Ky519kXirC/giphy.gif"}
