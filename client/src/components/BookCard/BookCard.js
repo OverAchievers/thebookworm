2cc92391fdf064d398c45fcebccacbef4c17d272
mrderrickguy <zakiahdg@gmail.com>
2018-05-07 18:56:15 -0500

    Made shadows behind buttons

--- client/src/components/BookCard/BookCard.js
+++ client/src/components/BookCard/BookCard.js
@@ -1,9 +1,9 @@
 import "./BookCard.css";
 import React, { Component } from "react";
-import { Col, Row, Container } from "../../components/Grid";
 //Libraries from react-bootstrap (npm install --save react-bootstrap )
 import Button from 'react-bootstrap/lib/Button';
 import BookBrowseModal from "../../components/BookBrowseModal";
+import BookManageModal from "../../components/BookManageModal";
 
 class BookCard extends Component {
     constructor(props, context) {
@@ -13,13 +13,20 @@ class BookCard extends Component {
             manageModalShow: false
         };
     };
-
+    tryStuff = () => {
+      this.props.source === "browse" ?
+      (
+        this.setState({browseModalShow:true})
+        ) : (
+        this.setState({manageModalShow:true})
+     )};
     render() {
 
         let browseModalOpen = () => this.setState({ browseModalShow: true });
         let browseModalClose = () => this.setState({ browseModalShow: false });
         let manageModalOpen = () => this.setState({ manageModalShow: true });
         let manageModalClose = () => this.setState({ manageModalShow: false });
+
 
         return (
             <div className="book">
@@ -37,15 +44,15 @@ class BookCard extends Component {
                     <Button
                         bsStyle="primary"
                         // onClick={() => this.setState({ modalShow: true })}
-                        onClick={browseModalOpen}>
+                        onClick={this.tryStuff}>
                         Details
                     </Button>
                 </div>
                 {this.props.source === "browse" ? (
-                    <BookBrowseModal visible={this.state.browseModalShow} onClickBackdrop={browseModalClose} book={this.props.book} />
+                    <BookBrowseModal visible={this.state.browseModalShow} onClickBackdrop={browseModalClose} onClose={browseModalClose} book={this.props.book} />
                   ) : (
-                    <BookBrowseModal visible={this.state.browseModalShow} onClickBackdrop={browseModalClose} book={this.props.book} />
-                )}               
+                    <BookManageModal visible={this.state.manageModalShow} onClickBackdrop={manageModalClose} book={this.props.book} />
+                )}
             </div>
         );
     }
--- client/src/pages/Manage/Manage.js
+++ client/src/pages/Manage/Manage.js
@@ -4,56 +4,68 @@ import { Col, Row, Container } from "../../components/Grid";
 import API from "../../utils/API";
 import UserIcon from "../../components/UserIcon";
 import BookCard from "../../components/BookCard";
+import DeleteBtn from "../../components/DeleteBtn";
 
 
 class Manage extends Component {
   state = {
-    books: []
+    books : []
   };
 
-  componentDidMount() {
-    this.loadBooks()
-  };
-
-  loadBooks = userId => {
-    API.getUserBooks("5aede203ea3cd12fc0e7c1c2")
-      .then(res => {
-        this.setState({ books: res.data })
-      })
-      .catch(err => console.log(err));
-  };
-
-  // handleInputChange = event => {
-  //   const { name, value } = event.target;
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // };
+componentDidMount(){
+  this.loadBooks();
+};
 
 
-  render() {
-    return (
-      <Container fluid>
-        <Row>
-          <Col size="lg-12 md-12 sm-12">
-            <hr></hr>
+loadBooks = userId => {
+  API.getUserBooks("5aedca116521850c59d4a680")
+  .then(res => {
+    this.setState({books:res.data})
+  })
+  .catch(err => console.log(err));
+};
+
+deleteBook = id => {
+  API.deleteBook(id)
+    .then(res => this.loadBooks())
+    .catch(err => console.log(err));
+};
+
+// handleInputChange = event => {
+//   const { name, value } = event.target;
+//   this.setState({
+//     [name]: value
+//   });
+// };
+
+
+render() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="lg-12 md-12 sm-12">
+          <hr></hr>
+        </Col>
+      </Row>
+      <Row>
+        {this.state.books.length ? (
+          <Col size="md-12 lg-12">
+            {this.state.books.map(book => (
+              <div>
+              <BookCard book={book} key={book._id} source={"manage"}>
+    
+              </BookCard>
+              </div>
+            ))}
           </Col>
-        </Row>
-        <Row>
-          {this.state.books.length ? (
-            <Col size="md-12 lg-12">
-              {this.state.books.map(book => (
-                <BookCard book={book} key={book._id}></BookCard>
-              ))}
-            </Col>
-          ) : (
-              <h3>No Results to Display</h3>
-            )}
-        </Row>
-        <div className="push"></div>
-      </Container>
-    );
-  }
+        ) : (
+          <h3>No Results to Display</h3>
+        )}
+      </Row>
+      <div className="push"></div>
+    </Container>
+  );
+}
 }
 
 export default Manage;
