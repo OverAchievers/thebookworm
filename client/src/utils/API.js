2a876a6376af9ec174f340fb2cb3dbc4d92c4a5b
natejahnke <nathanjjahnke@gmail.com>
2018-05-07 20:01:48 -0500

    added search as bookname and isbn in same search bar

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,7 +4,7 @@ export default {
   // Sample API1
   getBook: function (isbn) {
     return axios.get(
-      "https://www.googleapis.com/books/v1/volumes?q=isbn:" +
+      "https://www.googleapis.com/books/v1/volumes?q=" +
       isbn +
       "&key=AIzaSyBr1902s5dmz5vSbYgt67qn3x0HO7rMmzg"
     );
